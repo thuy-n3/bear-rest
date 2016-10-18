83a6033dea1f33242335a52bf879758c6f2910fe
Thuy Nguyen <ngtan3@gmail.com>
2016-10-18 17:32:03 -0500

    set up model - bear.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080; 
 
+//CONNECTING DATABASE
+//===============================================
+var mongoose = require('mongoose'); 
+mongoose.connect('mongodb://t3nguyen:n0d3b3@rs@ds053166.mlab.com:53166/bear-rest');
+
+
 //ROUTES FOR API 
 //===============================================
 var router = express.Router(); 
